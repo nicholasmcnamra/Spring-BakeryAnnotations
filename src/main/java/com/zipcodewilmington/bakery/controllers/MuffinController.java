9e3f26af8ace9829bfce62c68fcb27a44eff79dd
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-21 18:17:34 -0400

    even added a way to persist to locally hosted database - finished

--- src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/MuffinController.java
@@ -4,31 +4,33 @@ import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.services.MuffinService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class MuffinController {
     private MuffinService service;
 
     public MuffinController(MuffinService service) {
         this.service = service;
     }
-
+    @GetMapping("/muffins")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-
-    public ResponseEntity<Muffin> show(Long id) {
+    @GetMapping("/muffin/{id}")
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-
-    public ResponseEntity<Muffin> create(Muffin baker) {
-        return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
+    @PostMapping("/muffins")
+    public ResponseEntity<Muffin> create(@RequestBody Muffin muffin) {
+        return new ResponseEntity<>(service.create(muffin), HttpStatus.CREATED);
     }
-
-    public ResponseEntity<Muffin> update(Long id, Muffin baker) {
+    @PutMapping("/muffin/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-
-    public ResponseEntity<Boolean> destroy(Long id) {
+    @DeleteMapping("/muffin/{id}")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
